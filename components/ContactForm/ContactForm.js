1fa098e6bb70f407dd14260339e34aca18efcca1
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2020-06-04 19:52:43 +0100

    add form-name attribute

--- components/ContactForm/ContactForm.js
+++ components/ContactForm/ContactForm.js
@@ -2,7 +2,7 @@ import styles from "./ContactForm.module.scss";
 
 const ContactForm = () => {
   return (
-    <form name="contact" method="POST" data-netlify="true">
+    <form name="contact" form-name="contact" method="POST" data-netlify="true">
       <p>
         <label>
           Your Name: <input type="text" name="name" />
