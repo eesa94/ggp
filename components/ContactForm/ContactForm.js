e9ced6bad73f6a8465d9320ab5a9f10797aad217
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2020-06-07 15:05:22 +0100

    contact form styling, other minor changes

--- components/ContactForm/ContactForm.js
+++ components/ContactForm/ContactForm.js
@@ -7,33 +7,6 @@ const ContactForm = () => {
   return (
     <section className={styles.section}>
       <div className={styles.container}>
-        {/* <form
-          name="contact"
-          method="POST"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-        >
-          <input type="hidden" name="form-name" value="contact" />
-          <div className={styles.formGroup}>
-            <label>
-              Your Name: <input type="text" name="name" />
-            </label>
-          </div>
-          <div className={styles.formGroup}>
-            <label>
-              Your Email: <input type="email" name="email" />
-            </label>
-          </div>
-          <div className={styles.formGroup}>
-            <label>
-              Message: <textarea name="message"></textarea>
-            </label>
-          </div>
-          <div className={styles.formGroup}>
-            <button type="submit">Send</button>
-          </div>
-        </form> */}
-
         <form
           name="contact"
           method="POST"
@@ -41,7 +14,7 @@ const ContactForm = () => {
           data-netlify-honeypot="bot-field"
         >
           <input type="hidden" name="form-name" value="contact" />
-          <legend>Contact us</legend>
+          <h2 className={styles.header}>Contact us</h2>
 
           <Input
             label="First Name"
@@ -71,7 +44,7 @@ const ContactForm = () => {
             floatingLabel={true}
             required={true}
           />
-          <Button variant="raised" type="submit">
+          <Button type="submit" className={styles.button}>
             Submit
           </Button>
         </form>
